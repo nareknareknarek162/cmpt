ba3e56b87ebcffe1a35392ff0b850eda1aa5c909
Narek <narek7284@gmail.com>
2025-10-18 00:27:41 +0300

    added field options to comments/likes, hid db settings in env

--- config/settings.py
+++ config/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "competition_db",
-        "USER": "postgres",
-        "PASSWORD": "123456789",
+        "NAME": env("DB_NAME", cast=str),
+        "USER": env("DB_USER", cast=str),
+        "PASSWORD": env("DB_PASSWORD", cast=str),
         "HOST": "localhost",
         "PORT": "5432",
     }
--- models_app/models/like/models.py
+++ models_app/models/like/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 
 class Like(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
-    photo = models.ForeignKey("Photo", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+        related_name="likes",
+        related_query_name="like",
+        verbose_name="Автор",
+    )
+    photo = models.ForeignKey(
+        "Photo",
+        on_delete=models.CASCADE,
+        related_name="likes",
+        related_query_name="like",
+        verbose_name="Фото",
+    )
 
     class Meta:
         db_table = "likes"
