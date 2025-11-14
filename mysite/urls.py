c12584a484fe64a493ca020a02d073779c8f21a0
Narek <narek7284@gmail.com>
2025-11-14 16:58:58 +0300

    registration feature added

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("auth/", views.authorisation_page, name="auth"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("registration/", views.registration_view, name="registration"),
+    path("registrate/", views.registrate_view, name='registrate')
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
 
-from models_app.models import Photo
+from models_app.models import Photo, User
 
 
 def index(request):
@@ -28,4 +28,23 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
+    return redirect("index")
+
+
+def registration_view(request):
+    return render(request, "registrate.html")
+
+
+def registrate_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    first_name = request.POST["first_name"]
+    last_name = request.POST["last_name"]
+    email = request.POST["email"]
+    birth_date = request.POST["birth_date"]
+    gender = request.POST["gender"]
+    user = User(username=username, first_name=first_name, last_name=last_name, email=email, birth_date=birth_date,
+                is_staff=False, is_active=True, gender=gender)
+    user.set_password(password)
+    user.save()
     return redirect("index")
