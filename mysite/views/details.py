1a5b900e85fa7fd0e7a250088f9ea3ab3e57d016
Narek <narek7284@gmail.com>
2025-12-12 12:20:13 +0300

    added delete and working on edit features

--- mysite/views/details.py
+++ mysite/views/details.py
@@ -10,38 +10,50 @@ class DetailedPhotoView(DetailView):
     template_name = "details.html"
     context_object_name = "photo"
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         context["is_liked"] = Like.objects.filter(
             user=self.request.user, photo=self.get_object()
         ).exists()
         context["comments"] = Comment.objects.all()
+        context["has_commented"] = Comment.objects.filter(
+            author=self.request.user, photo=self.get_object()
+        ).exists()
         return context
 
     def post(self, request, *args, **kwargs):
         user = request.user
 
         if user.is_authenticated:
-            if "comment_text" in request.POST:
-                comment_text = request.POST["comment_text"]
-                comment = Comment(
-                    author=user,
-                    photo=self.get_object(),
-                    text=comment_text,
-                    comment_date=timezone.now(),
-                )  # auto_now_add=True
-                comment.save()
+            if "comment" in request.POST:
+                action = request.POST["comment"]
+
+                if action == "edit":
+                    pass
+
+                elif action == "save":
+                    comment_text = request.POST["comment_text"]
+                    comment = Comment(
+                        author=user,
+                        photo=self.get_object(),
+                        text=comment_text,
+                        comment_date=timezone.now(),
+                    )  # auto_now_add=True
+                    comment.save()
+
+                else:
+                    Comment.objects.filter(
+                        author=user, photo=self.get_object()
+                    ).delete()
 
             if "like" in request.POST:
-                if not user.is_authenticated:
-                    return redirect("auth")
 
                 obj, created = Like.objects.get_or_create(
                     user=user, photo=self.get_object()
                 )
 
                 if not created:
-                    Like.objects.filter(user=user, photo=self.get_object()).delete()
+                    obj.delete()
 
             return redirect("details", pk=self.get_object().pk)
         return redirect("auth")
