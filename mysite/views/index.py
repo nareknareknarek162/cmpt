efeb1eadd66091456c847f0b667104e2d844ff78
Narek <narek7284@gmail.com>
2025-12-17 21:15:38 +0300

    filter feature(username and description) done

--- mysite/views/index.py
+++ mysite/views/index.py
@@ -11,12 +11,32 @@ class IndexView(ListView):
     context_object_name = "photos"
 
     def get_queryset(self):
-        sorting_feature = self.request.GET.get("sort")
         query = super().get_queryset()
-        if sorting_feature == "likes":
-            return query.annotate(likes_count=Count("like")).order_by("-likes_count")
-        if sorting_feature == "comments":
-            return query.annotate(comments_count=Count("comment")).order_by(
-                "-comments_count"
-            )
+        # сортировка
+        if "sort" in self.request.GET:
+            sorting_feature = self.request.GET.get("sort")
+
+            if sorting_feature == "likes":
+                return query.annotate(likes_count=Count("like")).order_by(
+                    "-likes_count"
+                )
+            if sorting_feature == "comments":
+                return query.annotate(comments_count=Count("comment")).order_by(
+                    "-comments_count"
+                )
+            if sorting_feature == "publication_date":
+                return query.order_by("publication_date")
+
+        # фильтр
+        if "filter" in self.request.GET:
+            filter_feature = self.request.GET.get("filter")
+            filter_query_value = self.request.GET.get("filter_query")
+
+            if filter_feature == "username":
+                return query.filter(author__username__icontains=filter_query_value)
+            if filter_feature == "description":
+                return query.filter(description__icontains=filter_query_value)
+            if filter_feature == "title":
+                pass
+
         return query.order_by("publication_date")
