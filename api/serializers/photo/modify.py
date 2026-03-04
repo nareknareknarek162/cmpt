7ef5356adf547f62bd3c6c3586a076def8ff0eb5
Narek <narek7284@gmail.com>
2026-03-05 01:46:31 +0300

    polished photo related api

--- api/serializers/photo/modify.py
+++ api/serializers/photo/modify.py
@@ -3,7 +3,8 @@ from rest_framework.serializers import ModelSerializer
 from models_app.models import Photo
 
 
-class PhotoUpdateSerializer(ModelSerializer):
+class PhotoModifySerializer(ModelSerializer):
+
     class Meta:
         model = Photo
         fields = [
