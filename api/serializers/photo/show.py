ad427e4fc730b62d2cd11f9ee18d1ef2f5777f31
Narek <narek7284@gmail.com>
2026-03-05 13:52:59 +0300

    integrated api to index page

--- api/serializers/photo/show.py
+++ api/serializers/photo/show.py
@@ -6,4 +6,4 @@ from models_app.models import Photo
 class PhotoShowSerializer(ModelSerializer):
     class Meta:
         model = Photo
-        fields = ["id", "image", "title"]
+        fields = ["id", "image", "title", "description"]
