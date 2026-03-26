b14da25ca40c7f289aff2091fa90edb527792465
Narek <narek7284@gmail.com>
2026-03-26 17:00:41 +0300

    integrated comment showlist to details page

--- api/serializers/comment/show.py
+++ api/serializers/comment/show.py
@@ -1,9 +1,13 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from models_app.models import Comment
 
 
 class CommentShowSerializer(ModelSerializer):
+    author = serializers.CharField(source="author.username")
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M")
+
     class Meta:
         model = Comment
         fields = [
