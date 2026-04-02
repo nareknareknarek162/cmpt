b3fdf22407040976f8e260ea06da93bb5cececa3
Narek <narek7284@gmail.com>
2026-04-02 13:10:55 +0300

    made updated_at field readable

--- api/serializers/comment/show.py
+++ api/serializers/comment/show.py
@@ -7,6 +7,7 @@ from models_app.models import Comment
 class CommentShowSerializer(ModelSerializer):
     author = serializers.CharField(source="author.username")
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M")
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M")
 
     class Meta:
         model = Comment
