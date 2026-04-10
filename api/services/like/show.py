7bc32b984bc6352068b9510cfde0460f4ad4b90c
Narek <narek7284@gmail.com>
2026-04-10 03:53:25 +0300

    added photo existance validation to likes show service

--- api/services/like/show.py
+++ api/services/like/show.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import status
+from service_objects.errors import ValidationError
 from service_objects.services import ServiceWithResult
 
 from models_app.models import Like
@@ -7,9 +10,27 @@ from models_app.models import Like
 class LikesShowService(ServiceWithResult):
     id = forms.IntegerField(required=True)
 
+    custom_validations = ["_validate_photo_exist"]
+
     def process(self):
-        self.result = self._likes()
+        self.run_custom_validations()
+        if self.is_valid():
+            self.result = self._likes()
         return self
 
     def _likes(self):
-        return Like.objects.filter(photo_id=self.cleaned_data["id"])
+        return self._photo
+
+    @property
+    def _photo(self):
+        try:
+            return Like.objects.filter(photo_id=self.cleaned_data["id"])
+        except ObjectDoesNotExist:
+            return None
+
+    def _validate_photo_exist(self):
+        if not self._photo:
+            self.add_error(
+                "photo", ValidationError(message="Запрошенной фотографии не существует")
+            )
+            self.response_status = status.HTTP_404_NOT_FOUND
