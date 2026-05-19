3aabd80fee0e8081001539f24335c32aa3926711
Narek <narek7284@gmail.com>
2026-05-19 03:05:51 +0300

    add validation if photo exists before commenting

--- api/services/comment/create.py
+++ api/services/comment/create.py
@@ -1,8 +1,12 @@
+from functools import lru_cache
+
 from django import forms
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from rest_framework import status
 from service_objects.fields import ModelField
 from service_objects.services import ServiceWithResult
 
-from models_app.models import Comment, User
+from models_app.models import Comment, Photo, User
 
 
 class CommentCreateService(ServiceWithResult):
@@ -11,8 +15,12 @@ class CommentCreateService(ServiceWithResult):
     text = forms.CharField(required=True, min_length=2)
     parent_comment = forms.IntegerField(required=False, min_value=1)
 
+    custom_validations = ["_validate_photo_exist"]
+
     def process(self):
-        self.result = self._create_comment()
+        self.run_custom_validations()
+        if self.is_valid():
+            self.result = self._create_comment()
         return self
 
     def _create_comment(self):
@@ -24,3 +32,18 @@ class CommentCreateService(ServiceWithResult):
         )
 
         return comment
+
+    @property
+    @lru_cache
+    def _photo(self):
+        try:
+            return Photo.objects.get(id=self.cleaned_data["id"])
+        except ObjectDoesNotExist:
+            return None
+
+    def _validate_photo_exist(self):
+        if not self._photo:
+            self.add_error(
+                "photo", ValidationError(message="Запрошенной фотографии не существует")
+            )
+            self.response_status = status.HTTP_404_NOT_FOUND
