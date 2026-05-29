4ff4e5b064c01c4cdbc7bb83584f7576694497f3
Narek <narek7284@gmail.com>
2026-05-29 22:51:15 +0300

    added UI for sending mass notification

--- models_app/admin/user/resources.py
+++ models_app/admin/user/resources.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
+from django.shortcuts import render
+from django.urls import path
 
 from models_app.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    change_list_template = "admin/users_changelist.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+
+        custom_urls = [
+            path(
+                "broadcast/",
+                self.admin_site.admin_view(self.broadcast_view),
+                name="users_broadcast",
+            ),
+        ]
+
+        return custom_urls + urls
+
+    def broadcast_view(self, request):
+        return render(request, "admin/broadcast_message_page.html")
+
     list_display = ["username", "first_name", "last_name"]
 
     search_fields = ["username", "first_name", "last_name"]
