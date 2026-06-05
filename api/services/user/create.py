c93e14a7d91a4135f4f751eb965f2408fc730915
Narek <narek7284@gmail.com>
2026-06-05 22:50:46 +0300

    added validations to user create service

--- api/services/user/create.py
+++ api/services/user/create.py
@@ -5,9 +5,9 @@ from models_app.models import User
 
 
 class UserCreateService(ServiceWithResult):
-    username = forms.CharField(required=True)
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
+    username = forms.CharField(required=True, min_length=2, max_length=20)
+    first_name = forms.CharField(required=True, min_length=1, max_length=30)
+    last_name = forms.CharField(required=True, min_length=1, max_length=30)
     email = forms.EmailField(required=True)
     birth_date = forms.DateField(required=True)
     password = forms.CharField(required=True)
