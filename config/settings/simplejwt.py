d477d99c3c2488accb71473aca32b7ef60d7193e
Narek <narek7284@gmail.com>
2026-06-11 02:17:02 +0300

    changed timedeltas

--- config/settings/simplejwt.py
+++ config/settings/simplejwt.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
