fa5c759dddb260eeb3e83b07243f7306f6310b77
Narek <narek7284@gmail.com>
2026-06-11 15:18:52 +0300

    hide celery broker url with django-environ

--- config/settings/celery.py
+++ config/settings/celery.py
@@ -1,5 +1,7 @@
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+from config.settings.django import env
+
+CELERY_BROKER_URL = env("CELERY_BROKER_URL", cast=str)
+CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND", cast=str)
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
