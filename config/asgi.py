9cbb35c49c59b057c6fbde2cb91083af53e1fff3
Narek <narek7284@gmail.com>
2026-06-11 19:27:56 +0300

    change imports order

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,16 @@
 import os
 
-from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
-from notifications.routing import ws_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+from notifications.routing import ws_urlpatterns
+from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
 
 application = ProtocolTypeRouter(
     {
